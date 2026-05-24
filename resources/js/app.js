944275076a4f6812e2e02139a4ec7439dd40bbde
jauh05 <jauharfua05@gmail.com>
2026-05-24 17:34:50 +0700

    update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,21 @@
-//
+const demoTriggers = document.querySelectorAll('[data-demo-trigger]');
+const demoModal = document.querySelector('[data-demo-modal]');
+const demoCloseButtons = document.querySelectorAll('[data-demo-close]');
+
+demoTriggers.forEach((trigger) => {
+    trigger.addEventListener('click', () => {
+        demoModal?.classList.add('is-open');
+    });
+});
+
+demoCloseButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        demoModal?.classList.remove('is-open');
+    });
+});
+
+demoModal?.addEventListener('click', (event) => {
+    if (event.target === demoModal) {
+        demoModal.classList.remove('is-open');
+    }
+});
